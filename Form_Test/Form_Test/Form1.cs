c29e874dbd4b945400c34449b6c1a70c7a8926ee
kira4649 <mamimumetaru@gmail.com>
2025-10-24 15:35:05 +0900

    TestButtonを24個作成

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -15,6 +15,34 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    //インスタンスの生成
+                    TestButton testButton = new TestButton();
+
+                    //ボタンの位置設定
+                    testButton.Location = new Point(50 * i, 50 * j);
+
+                    //ボタンの大きさを設定
+                    testButton.Size = new Size(50, 50);
+
+                    //ボタン内のテキスト設定
+                    testButton.Text = "ボタン";
+
+                    //コントロールにボタンを追加
+                    Controls.Add(testButton);
+
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("最近寒いね");
         }
     }
 }
